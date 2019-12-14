a4e6a35a3ac1b0424f879962ba1d7314e12ee3e7
U-YOGA710\saman <samantoz@gmail.com>
2019-12-14 10:16:21 -0600

    update

--- assignment_1_SamantaArindam.R
+++ assignment_1_SamantaArindam.R
@@ -2,6 +2,21 @@
 
 # Name: SAMANTA, ARINDAM
 
-# Date: 04 DECEMBER 2019
+# Date: 08 DECEMBER 2019
 
 # TASK 1
+# The below code is just for a sample to test the export
+setwd("~/GitHub/BU-DSC")
+rm(list = ls())
+scores <- read.csv("scores.csv", header = TRUE)
+
+sink("assignment_1_SamantaArindam.txt")
+print(names(scores))
+print(summary(scores))
+sink()
+
+jpeg("histogram.jpg")
+pdf("histogram.pdf")
+# Code 
+
+graphics.off()
